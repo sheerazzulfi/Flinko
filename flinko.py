7fffc6fba205ad6c5e9a494c0c3d3bb61564d414
sheerazzulfi <97657986+sheerazzulfi@users.noreply.github.com>
2022-06-17 10:11:20 +0530

    Update flinko.py

--- flinko.py
+++ flinko.py
@@ -29,10 +29,6 @@ def login(mail, password):
     exid = out['responseObject']['id']
 
     time.sleep(2)
-
-    res = s.get('https://app.flinko.com:8110/optimize/v1/executionResponse/result/' + exid, headers=head)
-    cont = json.loads(res.content)
-    fres = cont['responseObject']['suiteStatus']
     sc = 0
     while (sc < 1):
         r1 = s.get('https://app.flinko.com:8110/optimize/v1/executionResponse/result/' + exid, headers=head)
