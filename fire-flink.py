a670883775f7c7cf3d546b0513c8e850ab86f940
sheerazzulfi <97657986+sheerazzulfi@users.noreply.github.com>
2022-11-16 16:36:24 +0530

    Update fire-flink.py

--- fire-flink.py
+++ fire-flink.py
@@ -20,33 +20,33 @@ def login(token):
     head["Authorization"] = "Bearer " + token
     suiteid = 'SUITE1002'
     baseUrl = 'https://preprod1.fireflink.com'
-    pes = s.post(baseUrl+':8081/dashboardexecution/optimize/v1/dashboard/execution/suite/PJT1001' + suiteid, headers=head, verify=False)
+    pes = s.post(baseUrl+':8081/dashboardexecution/optimize/v1/dashboard/execution/suite/' + suiteid, headers=head, verify=False)
     rus = s.post(baseUrl+':8081/project/optimize/v1/suite/runSetting/' + suiteid, headers=head, verify=False)
+    # ruo = json.loads(rus.content)
     out = json.loads(pes.content)
     exid = out['responseObject']['id']
 
     time.sleep(3)
     sc = 0
     while (sc < 1):
-        r1 = s.get(baseUrl+':8081/dashboardexecution/optimize/v1/dashboard/execution/' + exid, headers=head, verify=False)
+        r1 = s.get(baseUrl+':8081/dashboardexecution/optimize/v1/dashboard/execution/projects/PJT1001' + suiteid, headers=head, verify=False)
         c1 = json.loads(r1.content)
-        fr1 = c1['responseObject']['executionStatus']
+        fr1 = c1['responseObject'][0]['executionStatus']
+        # fr1 = c1['responseObject']['_id']
         print('status : ' + fr1 + '......')
-        if (fr1 == "Completed" or fr1 == "Terminated"):
+        if (fr1 == "Completed" or fr1 == "Cancelled"):
             if fr1 == 'Completed':
-                r2 = s.get(baseUrl+':8210/optimize/v1/executionResponse/result/' + exid, headers=head, verify=False)
-                c2 = json.loads(r2.content)
-                fr2 = c2['responseObject']['suiteStatus']
-                if fr2 == 'FAIL':
+                r2 = c1['responseObject'][0]['resultStatus']
+                if r2 == 'FAIL':
                     raise Test_Failed
-                elif fr2 == 'WARNING':
+                elif r2 == 'WARNING':
                     print('End Result : ' +'Warning')
-                elif fr2 == 'Aborted':
+                elif r2 == 'Aborted':
                     print('End Result : ' +'Aborted')
                 else:
                     print("End Result : " + 'Test Passed')
                 sc = 1
-            elif (fr1 == 'Terminated'):
+            elif (fr1 == 'Cancelled'):
                 print("End Result : " + fr1)
                 sc = 1
         time.sleep(10)
