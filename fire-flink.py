31d25772e7ea14fb479081bec240ca284ba15610
sheerazzulfi <97657986+sheerazzulfi@users.noreply.github.com>
2022-11-16 15:11:03 +0530

    Update fire-flink.py

--- fire-flink.py
+++ fire-flink.py
@@ -20,22 +20,22 @@ def login(token):
     head["Authorization"] = "Bearer " + token
     suiteid = 'SUITE1002'
     baseUrl = 'https://preprod1.fireflink.com'
-    pes = s.post(baseUrl+':8081/dashboardexecution/optimize/v1/dashboard/execution/suite/' + suiteid, headers=head, verify= False)
-    rus = s.post(baseUrl+':8081/project/optimize/v1/suite/runSetting/' + suiteid, headers=head)
-    ruo= json.loads(rus.content)
+    pes = s.post(baseUrl+':8081/dashboardexecution/optimize/v1/dashboard/execution/suite/' + suiteid, headers=head, verify=False)
+    rus = s.post(baseUrl+':8081/project/optimize/v1/suite/runSetting/' + suiteid, headers=head, verify=False)
+    ruo = json.loads(rus.content)
     out = json.loads(pes.content)
     exid = out['responseObject']['id']
 
     time.sleep(3)
     sc = 0
     while (sc < 1):
-        r1 = s.get(baseUrl+':8081/dashboardexecution/optimize/v1/dashboard/execution/' + exid, headers=head, verify= False)
+        r1 = s.get(baseUrl+':8081/dashboardexecution/optimize/v1/dashboard/execution/' + exid, headers=head, verify=False)
         c1 = json.loads(r1.content)
         fr1 = c1['responseObject']['executionStatus']
         print('status : ' + fr1 + '......')
         if (fr1 == "Completed" or fr1 == "Terminated"):
             if fr1 == 'Completed':
-                r2 = s.get(baseUrl+':8210/optimize/v1/executionResponse/result/' + exid, headers=head, verify= False)
+                r2 = s.get(baseUrl+':8210/optimize/v1/executionResponse/result/' + exid, headers=head, verify=False)
                 c2 = json.loads(r2.content)
                 fr2 = c2['responseObject']['suiteStatus']
                 if fr2 == 'FAIL':
